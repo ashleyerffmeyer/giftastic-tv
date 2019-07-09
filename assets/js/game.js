7e55fcd31a7a1222897f7a546058be5dccf27fd7
Ashley Erffmeyer <erffmea@gmail.com>
2019-07-09 14:54:13 -0500

    updated game functionality, html, and css

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,157 +1,161 @@
 //
 $(document).ready(function () {
 
-    // Initial array of TV Shows
-    var tvShows = ['arrested development', 'unbreakable kimmy schmidt', 'cheers', 'the office', 'stranger things', 'gilmore girls', 'parks and rec', '30 rock', 'dawson\'s creek', 'the oc', 'friends', 'fraiser', 'family guy', 'will and grace', 'felicity', 'the americans', 'game of thrones', 'twilight zone', 'six feet under', 'veep']
+    renderShowButtons(tvShows, 'showButton', '#buttons-view');
+    console.log("page loaded")
 
-    // Function to display TV Show names on buttons
-    var renderShowButtons = function () {
+})
+// Initial array of TV Shows
+var tvShows = ['arrested development', 'unbreakable kimmy schmidt', 'cheers', 'the office', 'stranger things', 'gilmore girls', 'parks and rec', '30 rock', 'dawson\'s creek', 'the oc', 'friends', 'fraiser', 'family guy', 'will and grace', 'felicity', 'the americans', 'game of thrones', 'twilight zone', 'six feet under', 'veep']
 
-        // Deleting the buttons prior to adding new shows; this is necessary otherwise repeat buttons will be produced
-        $("#buttons-view").empty();
+// Function to display TV Show names on buttons
+var renderShowButtons = function (tvShows, classToAdd, areaToAddTo) {
 
-        //Looping through array of movies
-        for (i = 0; i < tvShows.length; i++) {
+    // Deleting the buttons prior to adding new shows; this is necessary otherwise repeat buttons will be produced
+    $(areaToAddTo).empty();
 
-            // Dynamicaly generating buttons for each movie in the array
-            // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-            var a = $("<button>");
+    //Looping through array of movies
+    for (i = 0; i < tvShows.length; i++) {
 
-            // Adding a class
-            a.addClass("show");
+        // Dynamicaly generating buttons for each movie in the array
+        // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+        var a = $("<button>");
 
-            // Adding a data-attribute with a value of the TV show at index i
-            a.attr("data-name", tvShows[i]);
+        // Adding a class
+        a.addClass(classToAdd);
 
-            // Providing the button's text with a value of the TV show at index i
-            a.text(tvShows[i]);
+        // Adding a data-attribute with a value of the TV show at index i
+        a.attr("data-name", tvShows[i]);
 
-            // Adding the button to the HTML
-            $("#buttons-view").append(a);
-        }
+        // Providing the button's text with a value of the TV show at index i
+        a.text(tvShows[i]);
+
+        // Adding the button to the HTML
+        $(areaToAddTo).append(a);
     }
-    renderShowButtons();
 
-    // Event listener for all button elements
-    $(".show").on("click", function () {
 
-        // In this case, the "this" keyword refers to the button that was clicked
-        var show = $(this).attr("data-name");
+}
 
-        //test
-        console.log(show);
+// Event listener for all button elements
+$(document).on("click", '.showButton', function () {
 
-        // Constructing a URL to search Giphy for the name of the TV show
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            show + "&api_key=etsRIVN264F5s3wV2DrY1ddmyAZxSmDx&limit=10";
+    // In this case, the "this" keyword refers to the button that was clicked
+    var show = $(this).attr("data-name");
 
-        // test
-        console.log(queryURL);
+    //test
+    console.log(show);
 
-        // Performing our AJAX GET request
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
+    // Constructing a URL to search Giphy for the name of the TV show
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        show + "&api_key=etsRIVN264F5s3wV2DrY1ddmyAZxSmDx&limit=10";
 
-            // After the data comes back from the API
-            .then(function (response) {
+    // test
+    console.log(queryURL);
 
-                // Storing an array of results in the results variable
-                var results = response.data;
-                console.log(response);
+    // Performing our AJAX GET request
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
 
-                // Looping over every result item
-                for (var i = 0; i < results.length; i++) {
+        // After the data comes back from the API
+        .then(function (response) {
 
-                    results = response.data;
+            // Storing an array of results in the results variable
+            var results = response.data;
+            console.log(response);
 
-                    // Only taking action if the photo has an appropriate rating
-                    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+            // Looping over every result item
+            for (var i = 0; i < results.length; i++) {
 
-                        // Creating a div for the gif
-                        var gifDiv = $("<div>");
+                results = response.data;
 
-                        // Storing the result item's rating
-                        var rating = results[i].rating;
+                // Only taking action if the photo has an appropriate rating
+                if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
 
-                        //test
-                        console.log(rating);
+                    // Creating a div for the gif
+                    var gifDiv = $("<div>");
 
-                        // Creating a paragraph tag with the result item's rating
-                        var p = $("<p>").text("Rating: " + rating);
+                    // Storing the result item's rating
+                    var rating = results[i].rating;
 
-                        // Creating the animated gif url
-                        var animated = results[i].images.original.url;
+                    //test
+                    console.log(rating);
 
-                        // Creating the still gif url
-                        var still = results[i].images.original_still.url
+                    // Creating a paragraph tag with the result item's rating
+                    var p = $("<p>").text("Rating: " + rating);
 
-                        // Creating an image tag
-                        var showImage = $("<img>");
+                    // Creating the animated gif url
+                    var animated = results[i].images.original.url;
 
-                        // Giving the image tag an src attribute of a proprty pulled off the
-                        // result item
-                        showImage.attr("src", still);
-                        showImage.attr("data-still", still);
-                        showImage.attr("data-animated", animated)
-                        showImage.attr("data-state", "still");
-                        showImage.attr("searchImage");
+                    // Creating the still gif url
+                    var still = results[i].images.original_still.url;
 
-                        //test
-                        console.log(show);
+                    // Creating an image tag
+                    var showImage = $("<img>");
 
-                        // Appending the paragraph and personImage we created to the "gifDiv" div we created
-                        gifDiv.append(p);
-                        gifDiv.append(showImage);
+                    // Giving the image tag an src attribute of a proprty pulled off the
+                    // result item
+                    showImage.attr("src", still);
+                    showImage.attr("data-still", still);
+                    showImage.attr("data-animated", animated)
+                    showImage.attr("data-state", "still");
+                    showImage.addClass("searchImage");
 
-                        // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
-                        $("#gifs-appear-here").prepend(gifDiv);
-                    }
+                    //test
+                    console.log(show);
+
+                    // Appending the paragraph and personImage we created to the "gifDiv" div we created
+                    gifDiv.append(p);
+                    gifDiv.append(showImage);
+
+                    // Prepending the gifDiv to the "#gifs-appear-here" div in the HTML
+                    $("#gifs-appear-here").prepend(gifDiv);
                 }
+            }
 
-            });
-    });
-
-    // ----------------------------------------------------------------
-    // CLICK HANDLERS
-    // ----------------------------------------------------------------
-
-    // When click on gif with class .searchImage, perform the following actions...
-    $(document).on("click", ".searchImage", function () {
-
-        // Creating a variable state
-        var state = $(this).attr('data-state');
-
-        // If/else statement to determine which url to display and change the data-state
-        if (state === 'still') {
-            $(this).attr('src', $(this).data('data-animated'));
-            $(this).attr('data-state', 'animated');
-        }
-        else {
-            $(this).attr('src', $(this).data('data-still'));
-            $(this).attr('data-state', 'still');
-        }
-    });
-
-    // This function handles events where one button is clicked
-    $("#add-show").on("click", function (event) {
-        event.preventDefault();
-
-        // This line grabs the input from the textbox
-        var newShow = $("#show-input").eq(0).val();
-
-        // Adding the show from the textbox to our array
-        tvShows.push(newShow);
-
-        // test
-        console.log(tvShows);
-
-        // Calling renderShowButtons function which handles the processing of our movie array
-        renderShowButtons();
-
-        //
-        return false;
-    });
-
+        });
 });
+
+// ----------------------------------------------------------------
+// CLICK HANDLERS
+// ----------------------------------------------------------------
+
+// When click on gif with class .searchImage, perform the following actions...
+$(document).on("click", ".searchImage", function () {
+
+    // Creating a variable state
+    var state = $(this).attr('data-state');
+
+    // If/else statement to determine which url to display and change the data-state
+    if (state === 'still') {
+        $(this).attr('src', $(this).data('animated'));
+        $(this).attr('data-state', 'animated');
+    }
+    else {
+        $(this).attr('src', $(this).data('still'));
+        $(this).attr('data-state', 'still');
+    }
+});
+
+// This function handles events where one button is clicked
+$("#add-show").on("click", function (event) {
+    event.preventDefault();
+
+    // This line grabs the input from the textbox
+    var newShow = $("#show-input").eq(0).val();
+
+    // Adding the show from the textbox to our array
+    tvShows.push(newShow);
+
+    // test
+    console.log(tvShows);
+
+    // Calling renderShowButtons function which handles the processing of our movie array
+    renderShowButtons(tvShows, 'showButton', '#buttons-view');
+
+    //
+    return false;
+});
+
